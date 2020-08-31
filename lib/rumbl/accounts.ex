27043e6ddde6a094d0a69f7693f1a459656f155f
Simone D'Avico <simone.davico@welld.ch>
2020-08-31 17:15:41 +0200

    feat: add context for user mgmt, add views for user list and user detail

---
+++ lib/rumbl/accounts.ex
@@ -0,0 +1,25 @@
+defmodule Rumbl.Accounts do
+  @moduledoc """
+  The Accounts context.
+  """
+
+  alias Rumbl.Accounts.User
+
+  def list_users do
+    [
+      %User{id: "1", name: "Josè", username: "Carioca"},
+      %User{id: "2", name: "Pippo", username: "Pippi"},
+      %User{id: "3", name: "Donald", username: "Duck"}
+    ]
+  end
+
+  def get_user(id) do
+    Enum.find(list_users(), fn map -> map.id == id end)
+  end
+
+  def get_user_by(params) do
+    Enum.find(list_users(), fn map ->
+      Enum.all?(params, fn {key, val} -> Map.get(map, key) == val end)
+    end)
+  end
+end
