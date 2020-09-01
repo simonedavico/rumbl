dac9f1e1ff2d90b6ca6d946307bbb2291e10c9d6
Simone D'Avico <simone.davico@welld.ch>
2020-09-01 12:40:49 +0200

    feat: create user migration + schema + changeset

--- lib/rumbl/accounts.ex
+++ lib/rumbl/accounts.ex
@@ -3,23 +3,32 @@ defmodule Rumbl.Accounts do
   The Accounts context.
   """
 
+  alias Rumbl.Repo
   alias Rumbl.Accounts.User
 
   def list_users do
-    [
-      %User{id: "1", name: "Josè", username: "Carioca"},
-      %User{id: "2", name: "Pippo", username: "Pippi"},
-      %User{id: "3", name: "Donald", username: "Duck"}
-    ]
+    Repo.all(User)
+  end
+
+  def get_user!(id) do
+    Repo.get!(User, id)
   end
 
   def get_user(id) do
-    Enum.find(list_users(), fn map -> map.id == id end)
+    Repo.get(User, id)
   end
 
   def get_user_by(params) do
-    Enum.find(list_users(), fn map ->
-      Enum.all?(params, fn {key, val} -> Map.get(map, key) == val end)
-    end)
+    Repo.get_by(User, params)
+  end
+
+  def change_user(%User{} = user) do
+    User.changeset(user, %{})
+  end
+
+  def create_user(attrs \\ %{}) do
+    %User{}
+    |> User.changeset(attrs)
+    |> Repo.insert()
   end
 end
--- lib/rumbl_web/router.ex
+++ lib/rumbl_web/router.ex
@@ -16,8 +16,9 @@ defmodule RumblWeb.Router do
   scope "/", RumblWeb do
     pipe_through :browser
 
-    get "/users", UserController, :index
-    get "/users/:id", UserController, :show
+    # get "/users", UserController, :index
+    # get "/users/:id", UserController, :show
+    resources "/users", UserController, only: [:index, :show, :new, :create]
 
     get "/", PageController, :index
   end
